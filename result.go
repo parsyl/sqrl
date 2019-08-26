9552a7da0a952488eb63c509e16111bdd04f6d33
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2019-08-26 23:55:15 +0200

    fix: document api

--- result.go
+++ result.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 )
 
+// RowsAffected returns number of rows affected or error from query execution
 func RowsAffected(res sql.Result, err error) (int64, error) {
 	if err != nil {
 		return 0, err
@@ -12,6 +13,7 @@ func RowsAffected(res sql.Result, err error) (int64, error) {
 	return res.RowsAffected()
 }
 
+// LastInsertId returns last insert id or error from query execution
 func LastInsertId(res sql.Result, err error) (int64, error) {
 	if err != nil {
 		return 0, err
